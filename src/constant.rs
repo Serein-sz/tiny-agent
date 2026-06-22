1a3fbe7d21d6a11b92166aeeea71e59c3a6482d0
Qiang Wang <wq_0324@icloud.com>
2026-06-23 00:13:11 +0800

    feat: structured_output

---
+++ src/constant.rs
@@ -0,0 +1,2 @@
+// static MODEL: &'static str = "openai/gpt-oss-120b:free";
+pub static MODEL: &'static str = "deepseek-v4-flash";
---
+++ src/trace.rs
@@ -0,0 +1,17 @@
+
+use tracing_subscriber::{fmt, EnvFilter};
+use tracing_subscriber::prelude::*;
+
+pub fn init_tracing() {
+    let fmt_layer = fmt::layer()
+        .with_target(false);
+    
+    let filter_layer = EnvFilter::try_from_default_env()
+        .or_else(|_| EnvFilter::try_new("info"))
+        .unwrap();
+    
+    tracing_subscriber::registry()
+        .with(filter_layer)
+        .with(fmt_layer)
+        .init();
+}
